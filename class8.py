722d24a36adabb3602d6088d62f09eba22c48f95
Vivien-Shah <hfsthane7787@gmail.com>
2025-06-13 18:26:46 +0530

    Add files via upload

---
+++ class8.py
@@ -0,0 +1,17 @@
+# Activity 1 operator precedence
+'''v=4
+w=5
+x=8
+y=3
+z=7
+
+z=(v+w) *x/y
+print ("Value of (v+w) *x/y is",z )
+
+name="Vivien"
+age=0
+
+if name == "Vivien" or (name== "John" and age >= 2 ) :
+    print("Hello, Welcome")
+else: 
+    print("Good Bye")'''
